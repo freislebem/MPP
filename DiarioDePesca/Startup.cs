93ce7fdc712dee98953d442b2ade85a00130f0f1
freislebem <si.denis@gmail.com>
2019-08-04 21:38:26 -0300

    Tentativa da primeira migration

--- DiarioDePesca/Startup.cs
+++ DiarioDePesca/Startup.cs
@@ -37,7 +37,7 @@ namespace DiarioDePesca
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddDbContext<EspecieContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("EspecieContext")));
+                    options.UseMySql(Configuration.GetConnectionString("EspecieContext"), builder => builder.MigrationsAssembly("DiarioDePesca")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
