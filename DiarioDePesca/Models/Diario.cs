f9cf834720ce09787b460525b218b4acf5374c9b
freislebem <si.denis@gmail.com>
2019-08-06 13:23:02 -0300

    Novo css bootstrap e classe principal Diario

---
+++ DiarioDePesca/Models/Diario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DiarioDePesca.Models
+{
+    public class Diario
+    {
+        public int Id { get; set; }
+        public DateTime DataRegistro { get; set; }
+        //public DateTime DataPescaria { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fim { get; set; }
+        public string Local { get; set; }
+        public string PressaoA { get; set; }
+        public string MareCheia { get; set; }
+        public string MareVazia { get; set; }
+
+        public string CondicaoDia { get; set; }
+        public string Lua { get; set; }
+        public string CorAgua { get; set; }
+
+        public string Anzol { get; set; }
+        public string Obs { get; set; }
+    }
+    
+}
